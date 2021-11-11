5331c396164b3c4de6e557a8973d02ee8f71d661
HernanMorel <hmorel78@gmail.com>
2021-11-11 18:35:50 -0300

    Broke out some mongo routes into separate files, and began serving static assets to make the index.js file much lighter and easier to troubleshoot.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const path =require('path');
+const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const {campgroundSchema , reviewSchema } = require('./schemas.js');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -23,46 +20,23 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true,
 
 const app = express();
 
-app.engine('ejs', ejsMate);
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-const validateReview = (req,res,next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
-app.use('/campgrounds', campgrounds)
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render('home')
 });
 
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: {reviews: reviewId} });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 
 app.all("*", (req, res, next) => {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -26,7 +26,7 @@ router.get('/new', (req,res) => {
     res.render('campgrounds/new');
 })
 
-router.post('/', validateCampground, catchAsync(async (req,res, next) => {
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
@@ -36,15 +36,15 @@ router.post('/', validateCampground, catchAsync(async (req,res, next) => {
 router.get('/:id', catchAsync(async (req,res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', {campground});
-}))
+}));
 
 router.get('/:id/edit', catchAsync(async (req,res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', {campground});
-}));
+}))
 
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`)
 }));

8584ff36a403b9fbb519e828fd06f9b1fa39ed30
HernanMorel <hmorel78@gmail.com>
2021-11-17 19:24:18 -0300

    Began the authorization process through passport and mongodb

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,6 +30,7 @@ router.get('/new', isLoggedIn, (req,res) => {
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully Made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
@@ -37,7 +38,8 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 
 router.get('/:id', catchAsync(async (req,res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    console.log(campground);
     if(!campground){
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');

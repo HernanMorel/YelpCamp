8e5d613df1bd335d4c4a69a3cfd43335c9bd8555
HernanMorel <hmorel78@gmail.com>
2021-12-02 23:15:13 -0300

    reseeded the heroku app

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,13 @@
  const {places, descriptors} = require('./seedHelpers');
  const Campground = require('../models/campground');
 
+ if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+ }
 
- mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true })
+ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp_camp'
+
+ mongoose.connect( dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
      .then(() => {
          console.log("Database Connected")
      })

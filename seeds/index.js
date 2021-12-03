d087be3074b0546ce7aca1b51d398a79653b7daa
HernanMorel <hmorel78@gmail.com>
2021-12-03 00:30:16 -0300

    endless tweeks!

--- seeds/index.js
+++ seeds/index.js
@@ -2,14 +2,9 @@
  const cities = require('./cities');
  const {places, descriptors} = require('./seedHelpers');
  const Campground = require('../models/campground');
+ 
 
- if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config();
- }
-
- const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp_camp';
-
- mongoose.connect( dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+ mongoose.connect( 'mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true })
      .then(() => {
          console.log("Database Connected")
      })

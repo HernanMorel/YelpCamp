2fb8f6a5660564f645955d43288f32e7cf9bfac9
HernanMorel <hmorel78@gmail.com>
2021-12-03 00:39:05 -0300

    repopulating campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,7 @@
  const Campground = require('../models/campground');
  
 
- mongoose.connect( 'mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true })
+ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true })
      .then(() => {
          console.log("Database Connected")
      })
